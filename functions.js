c2e0ae3efa6738552764bfd6b26b982b6b83ceda
joseanfernandez <fernanj40@gmail.com>
2019-03-30 16:02:21 +0100

    hashtags saved

--- functions.js
+++ functions.js
@@ -1,7 +1,72 @@
-async function sayHello () {
-  return 'Hello World!'
+async function findHashtagId(name) {
+  const res = await client.search({
+    index: 'hashtags',
+    q: 'name:' + name
+  })
+
+  return res.hits.hits[0]._id
 }
 
+async function getHashtag(name, category) {
+  const url = urlOcupa2 + 'instagram/ig_hashtag_search?q=' + name + '&user_id=' + config.userid
+  const { payload } = await Wreck.get(url)
+  if (payload.length > 0) {
+    let payson = JSON.parse(payload)
+    return await saveHashtag(payson.id, name, category)
+  } else {
+    Log.error('empty')
+    return {}
+  }
+
+}
+
+async function populateHashtagIndex() {
+  Log.info(hashtags.fashion)
+  for (let i in hashtags.fashion) {
+    await getHashtag(hashtags.fashion[i], 'fashion')
+  }
+
+  for (let i in hashtags.fitness) {
+    await getHashtag(hashtags.fitness[i], 'fitness')
+  }
+
+  for (let i in hashtags.food) {
+    await getHashtag(hashtags.food[i], 'food')
+  }
+
+  for (let i in hashtags.tech) {
+    await getHashtag(hashtags.tech[i], 'tech')
+  }
+
+  for (let i in hashtags.travel) {
+    await getHashtag(hashtags.travel[i], 'travel')
+  }
+
+  return 'Completed!'
+}
+
+async function saveHashtag(id, name, category) {
+  await client.index({
+    index: 'hashtags',
+    type: '_doc',
+    id: id,
+    body: {
+      name: name,
+      category: category
+    }
+  }).then(res => {
+    res.result = 'created' ?  Log.info('#' + name + ' saved') : Log.error(message = 'Error')
+  })
+
+  return id
+
+}
+
+
+
 module.exports = {
-  sayHello
+  findHashtagId,
+  getHashtag,
+  populateHashtagIndex,
+  saveHashtag
 }
--- router.js
+++ router.js
@@ -1,26 +1,47 @@
 module.exports = [
   {
     method: 'GET',
-    path: '/',
+    path: '/hashtag/{name?}',
+    handler: async (request, h) => {
+      const name = request.params.name ? request.params.name : null
+      let res
+
+      try {
+        const hashtagId = await fun.findHashtagId(name)
+        res = hashtagId
+      } catch (ex) {
+        Log.error('Error while try to get #' + name)
+        res = await fun.getHashtag(name, '')
+      }
+
+      return res
+    }
+  },
+
+  {
+    method: 'GET',
+    path: '/populateHashtagIndex',
     handler: async (request, h) => {
       const path = request.path.slice(1, request.path.length)
+
       try {
-        return await fun.sayHello()
+        return await fun.populateHashtagIndex()
       } catch (ex) {
-        Log.error('Error while try to get ' + path)
-        return {}
+        Log.error('Error in ' + path)
+        return 'Something was wrong...'
       }
     }
   },
+
   {
     method: 'GET',
-    path: '/hashtag/{name?}',
+    path: '/saveHashtag/{name?}',
     handler: async (request, h) => {
       // const path = request.path.slice(1, request.path.length)
       const name = request.params.name ? request.params.name : null
 
       try {
-
+        await fun.saveHashtag(1, name)
       } catch (ex) {
         Log.error('Error while try to get #' + name)
         return {}
